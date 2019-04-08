0e2d23c15cae0503666ca399f807bb9a047d1e4e
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-04-08 19:27:43 +0200

    feat: added default layout

---
+++ src/modules/root/layouts/default/default.layout.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-default-layout',
+  templateUrl: './default.layout.html',
+  styleUrls: ['./default.layout.scss']
+})
+export class DefaultLayout implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
