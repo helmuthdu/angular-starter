2b88e02c0ffef85ae7df4070062a35c324b5a86c
Helmuth Saatkamp <helmuthdu@gmail.com>
2019-04-09 11:17:13 +0200

    feat: added search helper

---
+++ src/app/helpers/search.helper.ts
@@ -0,0 +1,24 @@
+import { Observable, SchedulerLike, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, startWith } from 'rxjs/operators';
+
+interface InputObservableOptions {
+  time?: number;
+  scheduler?: SchedulerLike;
+  minLength?: number;
+}
+/**
+ * Creates an observable variable to be used with a search input
+ * @return Observable<string>
+ */
+export const setupSearchObservable = (
+  subject: Subject<string>,
+  options: InputObservableOptions
+): Observable<string> => {
+  const { time = 400, scheduler, minLength = 3 } = options;
+  return subject.pipe(
+    debounceTime(time, scheduler),
+    filter(query => query.length >= minLength || query.length === 0),
+    distinctUntilChanged(),
+    startWith('')
+  );
+};
