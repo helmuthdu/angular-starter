e8674e06b5d1fa430d259e6278e752c43b5a763b
Helmuth Saatkamp <helmuth.saatkamp@mercedes-benz.io>
2021-06-03 08:49:03 +0200

    fix: removed deprecated methods

--- src/app/modules/root/stores/modules/counter/effects.ts
+++ src/app/modules/root/stores/modules/counter/effects.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { EMPTY } from 'rxjs';
 import { catchError, delay, map } from 'rxjs/operators';
 import { RootActionTypes } from './types';
 
 @Injectable()
 export class Effects {
-  @Effect()
-  autoCounter$ = this.actions$.pipe(
-    ofType(RootActionTypes.autoIncrement),
-    delay(1000),
-    map(() => ({ type: RootActionTypes.increment })),
-    catchError(() => EMPTY)
+  autoCounter$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(RootActionTypes.autoIncrement),
+      delay(1000),
+      map(() => ({ type: RootActionTypes.increment })),
+      catchError(() => EMPTY)
+    )
   );
 
   constructor(private actions$: Actions) {}
